a51f41d1aa5af27039b809b5327feb1eb92a036d
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2024-09-25 17:58:52 +0900

    Fixed: message.php

--- message.php
+++ message.php
@@ -1,9 +1,12 @@
 <?php
-$name = $_POST['name']
-$email = $_POST['email']
-$phone = $_POST['phone']
-$website = $_POST['website']
-$message = $_POST['message']
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$website = $_POST['website'];
+$message = $_POST['message'];
 
 echo $name;
 ?>
