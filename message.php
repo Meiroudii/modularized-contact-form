d0ba451ae96859c90dce1843dfd863695bba64b4
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2024-09-25 17:21:43 +0900

    Err: message.php 500

--- message.php
+++ message.php
@@ -1,12 +1,9 @@
 <?php
-ini_set('display_errors', 1); 
-error_reporting(E_ALL);
-	// lezzgooo
-	$name = $_POST['name']
-	$email = $_POST['email']
-	$phone = $_POST['phone']
-	$website = $_POST['website']
-	$message = $_POST['message']
+$name = $_POST['name']
+$email = $_POST['email']
+$phone = $_POST['phone']
+$website = $_POST['website']
+$message = $_POST['message']
 
-	echo $name;
+echo $name;
 ?>
